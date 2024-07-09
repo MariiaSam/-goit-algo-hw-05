151c896576b455022ea9c6a29a4b37b1cfe0a834
MariiaSam <kontragent@ukr.net>
2024-07-09 21:49:25 +0300

    fix

--- Task_2/main.py
+++ Task_2/main.py
@@ -2,7 +2,7 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    pattern = "\d*\.\d+"
+    pattern = " \d*\.\d+ "
     for match in re.finditer(pattern, text):
         yield float(match.group())
 
